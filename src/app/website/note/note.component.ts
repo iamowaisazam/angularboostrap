d8d3e9981fae322bdc834f5cf29397379415b520
iamowaisazam <iamowaisazam@gmail.com>
2024-09-28 12:25:25 +0500

    update

--- src/app/website/note/note.component.ts
+++ src/app/website/note/note.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FeaturePostComponent } from '../home/home-feature-post/feature-post.component';
 
 @Component({
   selector: 'app-note',
   standalone: true,
-  imports: [],
+  imports: [
+    FeaturePostComponent
+  ],
   templateUrl: './note.component.html',
   styleUrl: './note.component.css'
 })
